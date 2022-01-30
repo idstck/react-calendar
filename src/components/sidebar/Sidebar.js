6808b61eb4245585a5892d4a4104cd86c075b0e4
lunadiotic <ahmadhakim404@gmail.com>
2022-01-30 20:58:56 +0700

    membuat component calendar dan sidebar create event

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
 const Sidebar = () => {
-  return <div className='col-3'>Sidebar</div>
+  return (
+    <div className='col-3'>
+      <div className='d-grid gap-2'>
+        <button className='btn btn-primary'>Create New Task</button>
+      </div>
+      <div className='m-t-20 text-white'>
+        <br />
+        <div className='my-1 p-2 bg-primary'>Watch Movies</div>
+        <div className='my-1 p-2 bg-success'>Learn</div>
+        <div className='my-1 p-2 bg-danger'>Hangout</div>
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar
