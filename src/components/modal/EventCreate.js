01851307a85717829bd0a9415cc4c3f514ae4536
lunadiotic <ahmadhakim404@gmail.com>
2022-01-30 22:52:47 +0700

    membuat modal bootstrap di react js

---
+++ src/components/modal/EventCreate.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import EventForm from './EventForm'
+
+const EventCreate = () => {
+  return <EventForm />
+}
+
+export default EventCreate
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import EventCreate from '../modal/EventCreate'
 
 const Sidebar = () => {
   return (
     <div className='col-3'>
       <div className='d-grid gap-2'>
-        <button className='btn btn-primary'>Create New Task</button>
+        <button
+          data-bs-toggle='modal'
+          data-bs-target='#create-event'
+          className='btn btn-primary'
+        >
+          Create New Task
+        </button>
       </div>
       <div className='m-t-20 text-white'>
         <br />
@@ -12,6 +19,8 @@ const Sidebar = () => {
         <div className='my-1 p-2 bg-success'>Learn</div>
         <div className='my-1 p-2 bg-danger'>Hangout</div>
       </div>
+
+      <EventCreate />
     </div>
   )
 }
